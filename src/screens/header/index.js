ce632a3139ac904c136e9ed56dcb9393e4847b1b
dreplica <adejo.david@aiesec.net>
2020-06-03 22:30:23 +0100

    creating skeletal structure for design, implementing header section whuch would have a text, image and the search bar

---
+++ src/screens/header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Component,Image} from './style'
+export default function Header() {
+  return (
+      <Component>
+          <Intro />
+          <Image src='' alt='background'/>
+    </Component>
+  );
+}
---
+++ src/screens/header/intro.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Search from '../../components/search'
+
+import {Content, Text } from './style'
+
+export default function Intro() {
+  return (
+      <Content>
+          <Text>
+
+          </Text>
+          <Search />
+    </Content>
+  );
+}
